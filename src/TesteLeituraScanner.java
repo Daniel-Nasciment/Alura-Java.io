6d72740b70a0cf9fb516fedc0a81c8e93f91db52
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-11-23 15:05:35 -0300

    Iniciando com Scanner...

---
+++ src/TesteLeituraScanner.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TesteLeituraScanner {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner scanner = new Scanner(new File("arquivo.csv"));
+		
+		while(scanner.hasNextLine()) {
+			
+			String linha = scanner.nextLine();
+			System.out.println(linha);
+			
+		}
+		
+
+	}
+
+}
