17581b00b9b36208c7f769a3ac9d5848a4aa0c03
Daniel-Nasciment <daniel.fecaf@outlook.com>
2021-11-23 12:32:38 -0300

    Lendo arquivo, copiando e criando um novo

---
+++ src/TesteCopiarArquivo.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class TesteCopiarArquivo {
+
+	public static void main(String[] args) throws IOException {
+
+
+		// LEITURA
+		FileInputStream fis = new FileInputStream("lorem.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		
+		// ESCRITA
+		OutputStream fos = new FileOutputStream("lorem1.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		String linha = br.readLine();
+
+
+		while(linha != null) {
+			
+			bw.write(linha);
+			bw.newLine();
+			linha = br.readLine();
+			
+		}
+	
+
+		br.close();
+		bw.close();
+		
+	}
+
+}
