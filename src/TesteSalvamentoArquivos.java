207cf11798e41bd16610d98ac520f64f65086371
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-02-02 15:40:05 -0300

    Refatorando lógica de criação de pastas a partir de string

--- src/TesteSalvamentoArquivos.java
+++ src/TesteSalvamentoArquivos.java
@@ -8,27 +8,17 @@ public class TesteSalvamentoArquivos {
 
 		String sinistro = "T1602142";
 
-		// O RESULTADO É DEPOIS DO 5º ATÉ O 8º INDICE
-		String fim = sinistro.substring(5, 8);
+		String caminhoFormatado = String.format("/00%s/%s/%s", sinistro.substring(1, 2), sinistro.substring(2, 5), sinistro.substring(5, 8));
 
-		// O RESULTADO É DEPOIS DO 2º ATÉ O 5º INDICE
-		String meio = sinistro.substring(2, 5);
-
-		// O RESULTADO É DEPOIS DO 1º ATÉ O 2º INDICE
-		String inicio = sinistro.substring(1, 2);
-
-		String caminhoFormatado = String.format("/00%s/%s/%s", inicio, meio, fim);
-
-		System.out.println(inicio);
-		System.out.println(meio);
-		System.out.println(fim);
 		System.out.println(caminhoFormatado);
 
 		String property = System.getProperty("save.arq");	
 
 		File pasta = new File(property.concat(caminhoFormatado));
-		pasta.mkdirs();
+		boolean mkdirs = pasta.mkdirs();
 	
+		System.out.println(mkdirs);
+		
 
 		PrintWriter ps = new PrintWriter(pasta + "/lorem1.txt");
 		ps.flush();
