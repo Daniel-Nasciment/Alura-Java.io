1471b00f6066834988153b7340d084627b59de93
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-02-02 16:51:48 -0300

    Adicionando condicionais

--- src/TesteSalvamentoArquivos.java
+++ src/TesteSalvamentoArquivos.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 
 public class TesteSalvamentoArquivos {
 
@@ -8,24 +10,33 @@ public class TesteSalvamentoArquivos {
 
 		String sinistro = "T1602142";
 
-		String caminhoFormatado = String.format("/00%s/%s/%s", sinistro.substring(1, 2), sinistro.substring(2, 5), sinistro.substring(5, 8));
+		String caminhoFormatado = String.format("/00%s/%s/%s", sinistro.substring(1, 2), sinistro.substring(2, 5),
+				sinistro.substring(5, 8));
+
+		if (sinistro.length() == 9) {
+			caminhoFormatado = String.format("/0%s/%s/%s", sinistro.substring(1, 3), sinistro.substring(3, 6),
+					sinistro.substring(6, 9));
+		}
+
+		if (sinistro.length() == 10) {
+			caminhoFormatado = String.format("/%s/%s/%s", sinistro.substring(1, 4), sinistro.substring(4, 7),
+					sinistro.substring(7, 10));
+		}
 
 		System.out.println(caminhoFormatado);
 
-		String property = System.getProperty("save.arq");	
+		String property = System.getProperty("save.arq");
 
 		File pasta = new File(property.concat(caminhoFormatado));
 		boolean mkdirs = pasta.mkdirs();
-	
-		System.out.println(mkdirs);
-		
 
-		PrintWriter ps = new PrintWriter(pasta + "/lorem1.txt");
+		System.out.println(mkdirs);
+
+		File arquivo = new File("lorem.txt");
+
+		PrintWriter ps = new PrintWriter(pasta + "/" + arquivo);
 		ps.flush();
 		ps.close();
-
-
-		System.out.println(property);
 
 	}
 
